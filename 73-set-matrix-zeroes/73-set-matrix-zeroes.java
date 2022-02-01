727fff93629a2b7221412022bb2fe60115b4dbaa
saibargavnellepalli <80017755+saibargavnellepalli@users.noreply.github.com>
2022-02-01 23:03:05 +0530

    Time: 2 ms (62.95%), Space: 53.9 MB (5.13%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.java
@@ -1,37 +1,45 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
         
-  boolean[][] new_Matrix = new boolean[matrix.length][matrix[0].length];
 
+   
          for (int i=0;i<matrix.length;i++){
              for (int j=0;j<matrix[0].length;j++){
-                 if (matrix[i][j] == 0 && new_Matrix[i][j]!=true){
+                 if (matrix[i][j] == 0 ){
 
-                     makeRowColZero(matrix,new_Matrix,i,j);
+                     makeRowColZero(matrix,i,j);
 
                  }
              }
          }
 
-     
+        for (int i=0;i<matrix.length;i++){
+            for (int j=0;j<matrix[0].length;j++){
+               if (matrix[i][j]== Integer.MAX_VALUE+2)
+                   matrix[i][j] =0;
+            }
+
+        }
 
     }
 
-    private static void makeRowColZero(int matrix[][] ,boolean[][] new_matrix, int a, int b) {
+    private static void makeRowColZero(int matrix[][] , int a, int b) {
         //row
-        for (int i=0;i<new_matrix[0].length;i++){
-                if (matrix[a][i]!=0 && new_matrix[a][i]!=true) {
-                    new_matrix[a][i] = true;
-                    matrix[a][i] = 0;
+        for (int i=0;i<matrix[0].length;i++){
+                if (matrix[a][i]!=0 ) {
+
+                    matrix[a][i] = Integer.MAX_VALUE+2;
                 }
         }
 
         //column
-        for (int i=0;i<new_matrix.length;i++){
-            if (matrix[i][b]!=0 && new_matrix[i][b]!=true) {
-                new_matrix[i][b] = true;
-                matrix[i][b] = 0;
+        for (int i=0;i<matrix.length;i++){
+            if (matrix[i][b]!=0 ) {
+
+                matrix[i][b] = Integer.MAX_VALUE+2;
             }
         }
     }
 }
+
+
