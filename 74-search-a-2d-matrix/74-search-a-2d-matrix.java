30ad5c30d11827bff2c1de3f1fd90a99077f2773
saibargavnellepalli <80017755+saibargavnellepalli@users.noreply.github.com>
2022-02-09 18:22:36 +0530

    Time: 0 ms (100.00%), Space: 42.2 MB (16.07%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.java
@@ -1,15 +1,59 @@
 class Solution {
    public static boolean searchMatrix(int[][] matrix, int target) {
        
-        int n = matrix[0].length;
+//         int n = matrix[0].length;
        
-        for (int i=0;i<matrix.length;i++){
+//         for (int i=0;i<matrix.length;i++){
 
-            //check if target is present in the current row
-            if (target>=matrix[i][0]&&target<=matrix[i][n-1]){
-                for (int j=0;j<n;j++){
-                    if (matrix[i][j]==target) return true;
+//             //check if target is present in the current row
+//             if (target>=matrix[i][0]&&target<=matrix[i][n-1]){
+//                 for (int j=0;j<n;j++){
+//                     if (matrix[i][j]==target) return true;
+//                 }
+//             }
+//         }
+//         return false;
+//     }
+        int start =0,end =matrix.length-1;
+        int n = matrix[0].length-1;
+        int len = matrix[0].length-1;
+        n =n/2;
+
+        while (start<=end){
+            int mid = (start+end)/2;
+            if (matrix[mid][n]>target){
+                if (matrix[mid][0]<=target && matrix[mid][len]>=target){
+                    boolean flag =true;
+                    for (int i=0;i<matrix[0].length;i++){
+                        if (matrix[mid][i]==target) {
+                            flag = false;
+
+                            return true;
+                        }
+                    }
+                    if (flag==true) return false;
                 }
+                else {
+                    end = mid-1;
+                }
+            }
+            else if (matrix[mid][n]<target){
+                if (matrix[mid][0]<=target && matrix[mid][len]>=target){
+                    boolean flag =true;
+                    for (int i=0;i<matrix[0].length;i++){
+                        if (matrix[mid][i]==target) {
+                            flag = false;
+                            return true;
+                        }
+                    }
+                    if (flag==true) return false;
+                }
+                else {
+                    start= mid+1;
+                }
+            }
+            else {
+                if (matrix[mid][n]==target) return true;
             }
         }
         return false;
