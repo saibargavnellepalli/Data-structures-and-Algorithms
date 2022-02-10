537dc249285cbfa1f4167c55ef050c4dd39854c8
saibargavnellepalli <80017755+saibargavnellepalli@users.noreply.github.com>
2022-02-10 21:11:38 +0530

    Time: 4 ms (49.49%), Space: 56.7 MB (5.15%) - LeetHub

--- 169-majority-element/169-majority-element.java
+++ 169-majority-element/169-majority-element.java
@@ -1,21 +1,45 @@
 class Solution {
     public static int majorityElement(int[] arr) {
-        HashMap<Integer,Integer> hashMap = new HashMap<>();
-        int n = arr.length;
-        for (int i=0;i<arr.length;i++){
-            if (hashMap.containsKey(arr[i])){
-                hashMap.put(arr[i],hashMap.get(arr[i])+1);
-            }else {
-                hashMap.put(arr[i],1);
-            }
-        }
+//         HashMap<Integer,Integer> hashMap = new HashMap<>();
+//         int n = arr.length;
+//         for (int i=0;i<arr.length;i++){
+//             if (hashMap.containsKey(arr[i])){
+//                 hashMap.put(arr[i],hashMap.get(arr[i])+1);
+//             }else {
+//                 hashMap.put(arr[i],1);
+//             }
+//         }
       
-        int majority = n/2;
-        for (Integer key : hashMap.keySet()){
-            int value = hashMap.get(key);
-            if (value>majority) return key;
-        }
+//         int majority = n/2;
+//         for (Integer key : hashMap.keySet()){
+//             int value = hashMap.get(key);
+//             if (value>majority) return key;
+//         }
 
-        return -1;
+//         return -1;
+        
+        
+        //    private static int majorityElement(int[] arr) {
+     int n = arr.length;
+   int majority = n/2;
+   Arrays.sort(arr);
+   int i=0,j=0;
+   while (i<n && j<n){
+       if (arr[i]==arr[j]){
+           j++;
+       }
+       else {
+           if ((j-i)>majority){
+               return arr[i];
+           }
+           else {
+               i=j;
+           }
+       }
+   }
+   if (j==n) return arr[j-1];
+   return -1;
     }
 }
+
+    
