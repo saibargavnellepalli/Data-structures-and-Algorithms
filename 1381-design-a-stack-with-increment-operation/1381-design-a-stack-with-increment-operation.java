685f4bc71ce7f81d97f74cfbe4d4a89b26d90457
saibargavnellepalli <80017755+saibargavnellepalli@users.noreply.github.com>
2022-03-11 19:28:25 +0530

    Time: 6 ms (83.55%), Space: 50.8 MB (26.08%) - LeetHub

--- 1381-design-a-stack-with-increment-operation/1381-design-a-stack-with-increment-operation.java
+++ 1381-design-a-stack-with-increment-operation/1381-design-a-stack-with-increment-operation.java
@@ -1,37 +1,39 @@
-class CustomStack {
-        int n;
-        int arr[];
-    int ind=-1;
+class CustomStack { 
+    private int [] inc; 
+    private int [] stack;
+    private int size;
     
     public CustomStack(int maxSize) {
-        n = maxSize;
-        arr = new int[n];
-        
-        
+        stack = new int[maxSize];
+        inc = new int[maxSize]; 
+        size = 0;
     }
     
-    public void push(int x) {
-        if(ind+1<n){
-            arr[++ind] = x;
-        }
-        
+    public void push(int x) {  
+        if (size < stack.length) {
+            stack[size++] = x;
+        }     
     }
     
-    public int pop() {
-        if(ind>=0){
-          return arr[ind--];
+    public int pop (){
+       if(size > 0) {
+            int top = stack[--size] + inc[size];
+            if (size > 0) inc[size - 1] += inc[size];
+            inc[size] = 0;
+            return top;
         }
         return -1;
+    
+    }
+    public void increment(int k, int val) { 
+         if (k > size) k = size;
+        if (k > 0) inc[k - 1] += val; 
+        }
     }
     
-    public void increment(int k, int val) {
-        if(k>=n) k = n;
-        for(int i=0;i<k;i++){
-            arr[i]+=val;
-        }
+	/* The second method - Constant time operations */
         
-    }
-}
+
 
 /**
  * Your CustomStack object will be instantiated and called as such:
