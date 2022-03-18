6dafc8126dde838b3eda68391abc90aa8de0aca3
saibargavnellepalli <80017755+saibargavnellepalli@users.noreply.github.com>
2022-03-18 10:38:40 +0530

    Time: 1 ms (11.23%), Space: 41.6 MB (6.89%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int climbStairs(int n) {
+        return totalWays(0, n, new HashMap<Integer,Integer>());
+    }
+    public int totalWays(int currentStair, int  targetStair, HashMap<Integer,Integer> memo ){
+        if(currentStair==targetStair) return 1;
+        
+          if(currentStair>targetStair) return 0;
+        
+        //check if subproblem has already occured or not
+      //  int currentKey = currentStair;
+        
+        if(memo.containsKey(currentStair)) return memo.get(currentStair);
+        
+        int oneJump = totalWays(currentStair+1, targetStair,memo );
+        int twoJump = totalWays(currentStair+2, targetStair,memo );
+          
+        memo.put(currentStair,oneJump+twoJump);
+        return oneJump+twoJump;
+        
+    }
+}
