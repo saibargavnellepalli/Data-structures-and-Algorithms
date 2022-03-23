6f49b4218c0d79636269eb6725068132cec16110
saibargavnellepalli <80017755+saibargavnellepalli@users.noreply.github.com>
2022-03-23 22:47:20 +0530

    Time: 329 ms (41.99%), Space: 118.5 MB (5.06%) - LeetHub

--- 494-target-sum/494-target-sum.java
+++ 494-target-sum/494-target-sum.java
@@ -1,18 +1,23 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
-        return ways(nums,target,0);
+        return ways(nums,target,0,new HashMap<String,Integer>());
     }
     
-    public int ways(int[] nums,int target, int CI){
+    public int ways(int[] nums,int target, int CI,HashMap<String,Integer> memo){
         
         
         if(CI>=nums.length && target==0) return 1;
          if(CI>=nums.length && target!=0) return 0;
         
-       // if(target==0) return 1;
+        String currentKey = Integer.toString(target) +"-"+Integer.toString(CI);
         
-        int plus = ways(nums,target-nums[CI],CI+1);
-        int minus = ways(nums,target+nums[CI],CI+1);
+        if(memo.containsKey(currentKey))
+            return memo.get(currentKey);
+        
+        int plus = ways(nums,target-nums[CI],CI+1,memo);
+        int minus = ways(nums,target+nums[CI],CI+1,memo);
+        
+        memo.put(Integer.toString(target) +"-"+Integer.toString(CI),plus+minus);
         
         return plus+minus;
     }
