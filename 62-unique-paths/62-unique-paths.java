abe5ee4b966d2e97c20122ebba4b5852ea83e3d5
saibargavnellepalli <80017755+saibargavnellepalli@users.noreply.github.com>
2022-04-03 10:09:39 +0530

    Time: 0 ms (100.00%), Space: 39.1 MB (85.98%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -2,20 +2,20 @@ class Solution {
     public int uniquePaths(int m, int n) {
         
             Integer[][] dp = new Integer[m][n];
-        return totalWays(0,0,m,n, dp);
+        return totalWays(m-1,n-1,m,n, dp);
     }
     public int totalWays(int c1,int c2,int m, int n,Integer dp[][]){
-       if(c1==m || c2 == n)
+       if(c1<0 || c2 <0)
            return 0;
         
-        if(c1==m-1 || c2 == n-1)
+        if(c1==0 || c2 == 0)
             return 1;
         
         if(dp[c1][c2]!=null)
             return dp[c1][c2];
         
-        int down = totalWays(c1+1,c2,m,n,dp);
-        int right = totalWays(c1,c2+1,m,n,dp);
+        int down = totalWays(c1-1,c2,m,n,dp);
+        int right = totalWays(c1,c2-1,m,n,dp);
         
         
         dp[c1][c2] = down+right;
