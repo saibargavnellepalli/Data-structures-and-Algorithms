f74f2b68f9743868b0e1f0f81f76b4b9ddda270e
saibargavnellepalli <80017755+saibargavnellepalli@users.noreply.github.com>
2022-04-03 10:08:23 +0530

    Time: 0 ms (100.00%), Space: 41.5 MB (15.54%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -1,22 +1,26 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        return totalWays(0,0,m,n,new HashMap<String,Integer>());
+        
+            Integer[][] dp = new Integer[m][n];
+        return totalWays(0,0,m,n, dp);
     }
-    public int totalWays(int c1,int c2,int m, int n,HashMap<String,Integer>memo){
+    public int totalWays(int c1,int c2,int m, int n,Integer dp[][]){
        if(c1==m || c2 == n)
            return 0;
-        
-        String currentKey = c1+"-"+c2;
-        if(memo.containsKey(currentKey))
-            return memo.get(currentKey);
         
         if(c1==m-1 || c2 == n-1)
             return 1;
         
-        int down = totalWays(c1+1,c2,m,n,memo);
-        int right = totalWays(c1,c2+1,m,n,memo);
+        if(dp[c1][c2]!=null)
+            return dp[c1][c2];
         
-        memo.put(currentKey, down+right);
+        int down = totalWays(c1+1,c2,m,n,dp);
+        int right = totalWays(c1,c2+1,m,n,dp);
+        
+        
+        dp[c1][c2] = down+right;
+        
+       
         
         return down+right;
     }
