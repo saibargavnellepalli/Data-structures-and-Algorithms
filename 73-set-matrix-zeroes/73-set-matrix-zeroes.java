3636bd306d1ed08a60c27334a2a05b7c7113e86d
saibargavnellepalli <80017755+saibargavnellepalli@users.noreply.github.com>
2022-05-30 14:31:29 +0530

    Time: 1 ms (92.45%), Space: 43.8 MB (90.47%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.java
@@ -1,20 +1,22 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
         
-       ArrayList<String> zeroPlaces = new ArrayList<>();
-        
-      int temp[][] = new int[matrix.length][matrix[0].length];
+    
+     
         for(int i=0;i<matrix.length;i++){
             for(int j=0;j<matrix[0].length;j++){
-              temp[i][j] = matrix[i][j];
+            if(matrix[i][j] == 0){
+                 column(i,matrix);
+                 row(j,matrix);
+             }
                 
                 }
             }
-        for(int i=0;i<temp.length;i++){
-            for(int j=0;j<temp[0].length;j++){
-             if(temp[i][j] == 0){
-                 column(i,matrix);
-                 row(j,matrix);
+        
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+             if(matrix[i][j] == 0 || matrix[i][j] == Integer.MAX_VALUE+2){
+                matrix[i][j] = 0;
              }
                 
                 }
@@ -24,13 +26,17 @@ class Solution {
        
     private static void column(int a,int[][] matrix){
             for(int i=0;i<matrix[0].length;i++){
-                matrix[a][i] = 0;
+                if(matrix[a][i] != 0){
+                matrix[a][i] = Integer.MAX_VALUE+2;
+                }
             }
         }
              
          private void row(int b,int matrix[][]){
             for(int j=0;j<matrix.length;j++){
-                matrix[j][b] = 0;
+                 if(matrix[j][b] != 0){
+                matrix[j][b] = Integer.MAX_VALUE+2;
+                }
             }
          }
         }
