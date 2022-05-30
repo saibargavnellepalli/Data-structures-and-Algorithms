3192e26d0a4a84507a62798cd1d8ea458b5321ac
saibargavnellepalli <80017755+saibargavnellepalli@users.noreply.github.com>
2022-05-30 12:33:55 +0530

    Time: 2 ms (39.39%), Space: 54.4 MB (20.02%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.java
@@ -1,45 +1,41 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
         
-
-   
-         for (int i=0;i<matrix.length;i++){
-             for (int j=0;j<matrix[0].length;j++){
-                 if (matrix[i][j] == 0 ){
-
-                     makeRowColZero(matrix,i,j);
-
-                 }
-             }
-         }
-
-        for (int i=0;i<matrix.length;i++){
-            for (int j=0;j<matrix[0].length;j++){
-               if (matrix[i][j]== Integer.MAX_VALUE+2)
-                   matrix[i][j] =0;
-            }
-
-        }
-
-    }
-
-    private static void makeRowColZero(int matrix[][] , int a, int b) {
-        //row
-        for (int i=0;i<matrix[0].length;i++){
-                if (matrix[a][i]!=0 ) {
-
-                    matrix[a][i] = Integer.MAX_VALUE+2;
+       ArrayList<String> zeroPlaces = new ArrayList<>();
+        
+      int temp[][] = new int[matrix.length][matrix[0].length];
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+              temp[i][j] = matrix[i][j];
+                
                 }
-        }
-
-        //column
-        for (int i=0;i<matrix.length;i++){
-            if (matrix[i][b]!=0 ) {
-
-                matrix[i][b] = Integer.MAX_VALUE+2;
+            }
+        for(int i=0;i<temp.length;i++){
+            for(int j=0;j<temp[0].length;j++){
+             if(temp[i][j] == 0){
+                 column(i,matrix);
+                 row(j,matrix);
+             }
+                
+                }
+            }
+    }
+        
+       
+    private static void column(int a,int[][] matrix){
+            for(int i=0;i<matrix[0].length;i++){
+                matrix[a][i] = 0;
             }
         }
-    }
-}
+             
+         private void row(int b,int matrix[][]){
+            for(int j=0;j<matrix.length;j++){
+                matrix[j][b] = 0;
+            }
+         }
+        }
+    
+
+
 
 
