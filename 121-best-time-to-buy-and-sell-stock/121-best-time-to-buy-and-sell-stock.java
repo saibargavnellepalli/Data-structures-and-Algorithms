45ce9a910c2161876c2e59108f57b76405f8d570
saibargavnellepalli <80017755+saibargavnellepalli@users.noreply.github.com>
2022-05-30 18:22:54 +0530

    Time: 2 ms (86.92%), Space: 59 MB (84.45%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
@@ -1,38 +1,14 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        //page 8
-        //book2 (Sahil) - 10
-        
-        return profit(prices,0,1,1,new HashMap<String,Integer>());
-       
-    }
-    private int profit(int[] prices,int CD, int canBuy, int transactionCount,HashMap<String,Integer> memo){
-        
-        if(CD >= prices.length  || transactionCount==0 )
-            return 0;
-        
-        //memo
-        String currentKey = Integer.toString(CD)+"-"+Integer.toString(canBuy)+"-"+Integer.toString(transactionCount);
-        
-        if(memo.containsKey(currentKey))
-            return memo.get(currentKey);
-        
-        if(canBuy == 1){
-              int idle = profit(prices,CD+1,canBuy,transactionCount,memo);
-              int buy = -prices[CD]+profit(prices,CD+1,0,transactionCount,memo);
-            memo.put(currentKey,  Math.max(buy,idle));
-            return Math.max(buy,idle);
-        }
-        else{
-              int idle = profit(prices,CD+1,canBuy,transactionCount,memo);
-        int sell =  prices[CD]+profit(prices,CD+1,1,transactionCount-1,memo);
-             memo.put(currentKey,  Math.max(sell,idle));
-            return Math.max(idle,sell);
-            
-            }
-        
-        
-     
+      int ans = 0,min=prices[0];
+      for(int i=1;i<prices.length;i++){
+          
+         ans = Math.max(prices[i]-min,ans);
+          min = Math.min(prices[i],min);
+      }
+        return ans;
       
     }
-}
+   
+    
+    }
