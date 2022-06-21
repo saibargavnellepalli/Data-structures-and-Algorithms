9f9e0304ad0013dc46cb1af04f3cfb5ec285bf14
saibargavnellepalli <80017755+saibargavnellepalli@users.noreply.github.com>
2022-06-21 18:47:36 +0530

    Time: 9 ms (98.20%), Space: 41.6 MB (91.03%) - LeetHub

---
+++ 9-palindrome-number/9-palindrome-number.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        String s = Integer.toString(x);
+        int i=0,j=s.length()-1;
+        while(i<=j){
+            if(s.charAt(i)!=s.charAt(j))
+                return false;
+            i++;j--;
+        }
+        return true;
+    }
+}
