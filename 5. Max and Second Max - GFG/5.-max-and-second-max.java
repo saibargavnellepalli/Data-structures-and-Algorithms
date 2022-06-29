a00d4f388a4388a8ba015b6569cfa4b345d3b71b
saibargavnellepalli <80017755+saibargavnellepalli@users.noreply.github.com>
2022-06-29 11:57:39 +0530

    Added solution - LeetHub

---
+++ 5. Max and Second Max - GFG/5.-max-and-second-max.java
@@ -0,0 +1,70 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+
+
+ // } Driver Code Ends
+        
+
+
+// Solution class to implement function largestAndSecondLargest
+class Solution{
+    
+    // Function to find largest and second largest element in the array
+    public static ArrayList<Integer> largestAndSecondLargest(int sizeOfArray, int arr[])
+    {
+        //code here.
+        ArrayList<Integer> al = new ArrayList<Integer>();
+        int m1=-1;
+        int m2=-1;
+        for(int a : arr){
+            if(a>m1){
+                m2=m1;
+                m1=a;
+            }
+            else if(a>m2 && a<m1){
+                m2=a;
+            }
+        }
+     al.add(m1);
+     al.add(m2);
+     return al;
+    }
+}
+
+// { Driver Code Starts.
+
+// Driver class
+class Array {
+    
+    // Driver code
+	public static void main (String[] args) throws IOException{
+		// Taking input using buffered reader
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int testcases = Integer.parseInt(br.readLine());
+		
+		// looping through all testcases
+		while(testcases-- > 0){
+		    int sizeOfArray = Integer.parseInt(br.readLine());
+		    
+		    int arr[] = new int[sizeOfArray];
+		    
+		    String line = br.readLine();
+		    String[] elements = line.trim().split("\\s+");
+		    
+		    for(int index = 0;index < sizeOfArray; index++){
+		        arr[index] = Integer.parseInt(elements[index]);
+		    }
+		    
+		    Solution obj = new Solution();
+		    ArrayList<Integer> res = new ArrayList<Integer>();
+		    res = obj.largestAndSecondLargest(sizeOfArray, arr);
+		    System.out.println(res.get(0) + " " + res.get(1) );
+		}
+	}
+}  // } Driver Code Ends
