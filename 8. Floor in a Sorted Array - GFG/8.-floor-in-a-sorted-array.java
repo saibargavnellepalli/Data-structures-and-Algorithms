dd9a872e73d7476e50eec11fd4c4e064c3df0ed1
saibargavnellepalli <80017755+saibargavnellepalli@users.noreply.github.com>
2022-07-05 23:09:03 +0530

    Added solution - LeetHub

--- 8. Floor in a Sorted Array - GFG/8.-floor-in-a-sorted-array.java
+++ 8. Floor in a Sorted Array - GFG/8.-floor-in-a-sorted-array.java
@@ -38,12 +38,17 @@ class Solution{
     // n is the size of array
     static int findFloor(long arr[], int n, long x)
     {
+        int st=0,end=n-1;
         int ans = -1;
-        for(int i=0;i<n;i++){
-            if(arr[i]<=x)
-                ans = i;
-                else
-                return ans;
+        while(st<=end){
+            int mid = st+(end-st)/2;
+            if(arr[mid]<=x){
+                ans = mid;
+                st=mid+1;
+            }
+            else{
+                end=mid-1;
+            }
         }
         return ans;
     }
