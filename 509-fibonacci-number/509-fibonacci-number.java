15f6dbc58be49fbdb4f90e63ea534c09a2ba9c30
saibargavnellepalli <80017755+saibargavnellepalli@users.noreply.github.com>
2022-07-06 21:22:34 +0530

    Time: 23 ms (5.20%), Space: 40.2 MB (81.42%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -1,19 +1,9 @@
 class Solution {
     public int fib(int n) {
-        return fibo(n, new HashMap<Integer,Integer>());
-    }
-        public int fibo(int n, HashMap<Integer,Integer> memo){
-        if(n<=1) return n;
-            
-            int currentKey = n;
-            if(memo.containsKey(currentKey))
-                return memo.get(currentKey);
-            
-        int decreaseByOne = fibo(n-1,memo);
-        int increaseByOne = fibo(n-2,memo);
+       if(n==0 || n==1)
+           return n;
         
-            memo.put(currentKey,decreaseByOne+increaseByOne);
-        return decreaseByOne+increaseByOne;
+        return fib(n-1)+fib(n-2);
 }
         
     
