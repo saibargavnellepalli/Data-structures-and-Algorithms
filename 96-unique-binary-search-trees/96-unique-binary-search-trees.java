a7bdc9290b6cbfa6e82712b2ae058ce141eb4980
saibargavnellepalli <80017755+saibargavnellepalli@users.noreply.github.com>
2022-07-11 12:26:48 +0530

    Time: 1 ms (17.05%), Space: 40.8 MB (56.91%) - LeetHub

--- 96-unique-binary-search-trees/96-unique-binary-search-trees.java
+++ 96-unique-binary-search-trees/96-unique-binary-search-trees.java
@@ -1,16 +1,21 @@
 class Solution {
     public int numTrees(int n) {
+       
+       // Integer arr[] = new Integer[200];
+       return total(n,new HashMap<Integer,Integer>());
+    }
+    private int total(int n, HashMap<Integer,Integer> map){
+    if(n==0 || n==1)
+       return 1;
+  
+    if(map.containsKey(n))
+        return map.get(n);
+    int ways = 0;
         
-        //catalan
-        return totalWays(n);
+    for(int i=0;i<n;i++){
+       ways+= total(i,map)*total(n-i-1,map);
     }
-    public int totalWays(int n){
-        if(n==0 || n==1)
-            return 1;
-        int ans =0;
-        for(int i=0;i<n;i++){
-            ans+=totalWays(i)*totalWays(n-i-1);
-        }
-        return ans;
-    }
+        map.put(n,ways);
+    return ways;
+}
 }
