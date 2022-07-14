725da521a6de8f8ac6a7d6371605ccf6c49b5591
saibargavnellepalli <80017755+saibargavnellepalli@users.noreply.github.com>
2022-07-14 18:15:31 +0530

    Added solution - LeetHub

---
+++ Boolean Matrix - GFG/boolean-matrix.java
@@ -0,0 +1,103 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        
+        while(t-- > 0)
+        {
+            String str[] = read.readLine().trim().split("\\s+");
+            int r = Integer.parseInt(str[0]);
+            int c = Integer.parseInt(str[1]);
+            int matrix[][] = new int[r][c];
+            
+            for(int i = 0; i < r; i++)
+            {
+                int k = 0;
+                str = read.readLine().trim().split("\\s+");
+                for(int j = 0; j < c; j++){
+                  matrix[i][j] = Integer.parseInt(str[k]);
+                  k++;
+                }
+            }
+            new Solution().booleanMatrix(matrix);
+            StringBuilder sb = new StringBuilder();
+            for(int i = 0; i < r; i++){
+                for(int j = 0; j < c; j++){
+                    sb.append(matrix[i][j] + " ");
+                }
+                sb.append("\n"); 
+            }
+            System.out.print(sb);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    //Function to modify the matrix such that if a matrix cell matrix[i][j]
+    //is 1 then all the cells in its ith row and jth column will become 1.
+    void booleanMatrix(int matrix[][])
+    {
+        // code here 
+          int r=matrix.length;
+       int c=matrix[0].length;
+       boolean row=false;
+       boolean col=false;
+       
+       for(int j=0;j<c;j++){
+           if(matrix[0][j]==1){
+               row=true;
+               break;
+           }
+       }
+       for(int i=0;i<r;i++){
+           if(matrix[i][0]==1){
+               col=true;
+               break;
+           }
+       }
+       
+       for(int i=1;i<r;i++){
+           for(int j=1;j<c;j++){
+               if(matrix[i][j]==1){
+                   matrix[0][j]=1;
+                   matrix[i][0]=1;
+               }
+           }
+       }
+       
+       for(int i=1;i<r;i++){
+           for(int j=1;j<c;j++){
+               if(matrix[0][j]==1 || matrix[i][0]==1){
+                   matrix[i][j]=1;
+               }
+           }
+       }
+       
+       
+           
+       if(row==true){
+           for(int j=0;j<c;j++){
+               matrix[0][j]=1;
+           }
+       }    
+        
+       if(col==true){
+           for(int i=0;i<r;i++){
+               matrix[i][0]=1;
+           }
+       }     
+    }
+}
