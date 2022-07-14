f1a374cabe4f22cb5ff419df7dd8477c8675075b
saibargavnellepalli <80017755+saibargavnellepalli@users.noreply.github.com>
2022-07-14 20:18:41 +0530

    Added solution - LeetHub

---
+++ 3. Binary String - GFG/3.-binary-string.java
@@ -0,0 +1,35 @@
+// { Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG{
+    public static Scanner sc = new Scanner(System.in);
+	public static void main (String[] args){
+        int t;
+    	t=sc.nextInt();
+    	sc.nextLine();
+    	while((t--)!=0){
+    	    int n=sc.nextInt();
+    	    sc.nextLine();
+    	    String s = new String();
+    	    s=sc.nextLine();
+    	    Solution obj = new Solution();
+    	    System.out.println(obj.binarySubstring(n, s));
+    	}
+	}
+}// } Driver Code Ends
+
+
+
+
+class Solution
+{
+    //Function to count the number of substrings that start and end with 1.
+    public static int binarySubstring(int a, String str)
+    {
+         String ans = str.replaceAll("0", "");
+        int n = ans.length();
+        return n * (n-1) / 2 ;
+    }
+}
