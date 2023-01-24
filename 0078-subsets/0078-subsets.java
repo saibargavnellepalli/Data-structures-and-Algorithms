fccbe0b8942f36967c368ae30ec8b3c31d02c38d
saibargavnellepalli <80017755+saibargavnellepalli@users.noreply.github.com>
2023-01-24 19:10:47 +0530

    Time: 2 ms (25.14%), Space: 43.1 MB (33.77%) - LeetHub

---
+++ 0078-subsets/0078-subsets.java
@@ -0,0 +1,36 @@
+class Solution {
+    Set<List<Integer>> ans = new HashSet<>();
+    public List<List<Integer>> subsets(int[] nums) {
+        
+        List<Integer> list = new ArrayList<>();
+        
+        combinations(0,0,nums,list);
+        return new ArrayList<>(ans);
+    }
+    
+    public void combinations(int i, int sum, int[] arr, List<Integer> list){
+        
+        //System.out.println(sum);
+           
+        if(i==arr.length){
+           ans.add(new ArrayList<>(list));
+            return;
+        }
+        
+        
+            
+       
+        
+     
+        
+        list.add(arr[i]);
+      
+        combinations(i+1,sum,arr,list);
+        
+        int n = list.size();
+       
+        list.remove(n-1);
+        
+        combinations(i+1,sum,arr,list);
+    }
+}
