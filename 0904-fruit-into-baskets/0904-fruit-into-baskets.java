5ed2abdcd1c66cde1a2b19eca17a4fa98d898f13
saibargavnellepalli <80017755+saibargavnellepalli@users.noreply.github.com>
2023-02-07 14:23:44 +0530

    Time: 58 ms (49.93%), Space: 51.8 MB (56.10%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.java
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.java
@@ -1,40 +1,43 @@
 class Solution {
     public int totalFruit(int[] fruits) {
         
-        int acq=0, rel=0, n=fruits.length;
-        HashMap<Integer, Integer> map = new HashMap<>();
-        int ans = Integer.MIN_VALUE;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int n = fruits.length;
+        int windowStart = 0,ans=0;
         
-        while(acq<n && rel<n){
+        for(int i=0;i<n;i++){
             
-                // ans = Math.max(acq-rel, ans);
-                
-                if(map.containsKey(fruits[acq])){
-                     map.put(fruits[acq],map.get(fruits[acq])+1);
-                }
-                else{
-                    map.put(fruits[acq], 1);
-                }
-                
-            if(map.size() <= 2)
-                ans = Math.max(acq-rel+1, ans);
             
-                if(map.size() > 2){
-              
-                    while(map.size() > 2){
-                         map.put(fruits[rel], map.get(fruits[rel])-1);
-                    
-                         if(map.get(fruits[rel]) == 0)
-                             map.remove(fruits[rel]);
-                    
-                         rel += 1;
-                }
+        
+               
+            
+            if(!map.containsKey(fruits[i])){
+                map.put(fruits[i],1);
+            }
+            else{
+                int val = map.get(fruits[i]);
+                map.put(fruits[i],val+1);
             }
             
+            if(map.size()==2){
+                ans = Math.max(ans,i-windowStart+1);
+            }
             
-            acq += 1;
+             while(map.size()>2){
+                ans = Math.max(ans, i-windowStart);
+                int val = map.get(fruits[windowStart]);
+                map.put(fruits[windowStart],val-1);
+                if(val-1 <=0){
+                    map.remove(fruits[windowStart]);
+                }
+                    windowStart ++;
+            }
+        
+            
+            
         }
         
-        return ans;
+       
+        return windowStart == 0 ? n : ans;
     }
 }
