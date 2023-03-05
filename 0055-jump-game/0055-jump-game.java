990adaac7316dbadc32d6edc9e89345ea1558700
saibargavnellepalli <80017755+saibargavnellepalli@users.noreply.github.com>
2023-03-05 19:07:11 +0530

    Time: 590 ms (9.20%), Space: 43.6 MB (18.88%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,32 +1,39 @@
 class Solution {
+    ArrayList<Integer> al = new ArrayList<>();
     public boolean canJump(int[] nums) {
-        Boolean[] dp = new Boolean[nums.length];
-        return isPossible(0,nums, dp);
+      Boolean[] dp = new Boolean[nums.length];
+        return jump(0,nums,dp);
+       
     }
-    public boolean isPossible(int CI, int[] nums, Boolean dp[]){
+    
+    // 1) -> 1->arr[i]
+    public boolean jump(int ind,int nums[], Boolean[] dp ){
         
-        if(CI==nums.length-1)
+        if(ind == nums.length-1) {
+            
             return true;
+        }
         
-        int currentKey = CI;
-        if(dp[CI] !=  null)
-            return dp[CI];
+        if(ind >= nums.length) return false;
         
-        int currentJump = nums[CI];
+        if(dp[ind] != null) return dp[ind];
+        
         boolean ans = false;
         
-        
-        
-        for(int i=1;i<=currentJump;i++){
-            boolean tempAns = isPossible(CI+i, nums,dp);
-            if (tempAns){
-                 dp[CI] = true;
+        for(int i=1;i<=nums[ind];i++){
+           
+                //al.add(i);
+            boolean t = jump(ind+i,nums,dp);
+            if(t == true){
+            dp[ind] = t ;
                 return true;}
-            ans = ans || tempAns;
+            
+           ans = ans || t;
+               
+            
         }
-        dp[CI] = ans;
+        dp[ind] = ans; 
         
         return ans;
-        
     }
 }
