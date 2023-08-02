0d53fa31cd4fe4e5a755f3041b7f5145f5c4b78b
Sai bargav Nellepalli <80017755+saibargavnellepalli@users.noreply.github.com>
2023-08-02 20:55:37 +0530

    Time: 0 ms (100.00%), Space: 41.3 MB (76.75%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.java
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -8,23 +8,37 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
+// node 1 -> #30129
+// node 2 -> #45350
+// node 3 -> #45351
+// node 4 -> #30122
+// node 5 -> #45353
+
+
+// ListNode newNode = new ListNode(5);
+
+// newNode -> #84899
+
 class Solution {
     public ListNode reverseList(ListNode head) {
       
+        
         ListNode ans = null;
-        ListNode t = head;
         
         while(head != null){
-        
+            
             ListNode temp = head.next;
             
             head.next = ans;
-            
             ans = head;
             
             head = temp;
+            
         }
-        
         return ans;
+            
+        
+            
     }
 }
