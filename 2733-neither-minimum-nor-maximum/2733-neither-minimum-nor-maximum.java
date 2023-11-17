b72e0129fdb3fd31d53050c55a25bff53e50dc14
Sai bargav Nellepalli <80017755+saibargavnellepalli@users.noreply.github.com>
2023-11-17 18:10:48 +0530

    Time: 4 ms (99.57%), Space: 44.4 MB (7.71%) - LeetHub

--- 2733-neither-minimum-nor-maximum/2733-neither-minimum-nor-maximum.java
+++ 2733-neither-minimum-nor-maximum/2733-neither-minimum-nor-maximum.java
@@ -1,9 +1,30 @@
 class Solution {
     public int findNonMinOrMax(int[] nums) {
-       if(nums.length <= 2) return -1;
+       
         
-        Arrays.sort(nums);
+       int minElm = nums[0];
+        int n = nums.length;
         
-        return nums[1];
+        for(int i=0;i<n;i++){
+            if(nums[i] < minElm){
+                minElm = nums[i];
+            }    
+        }
+        
+        int maxElm = nums[0];
+        for(int i=0;i<n;i++){
+            if(nums[i] > maxElm){
+                maxElm = nums[i];
+            }
+        }
+        
+        for(int i=0;i<n;i++){
+            if(nums[i] != minElm && nums[i] != maxElm){
+                return nums[i];
+            }
+        }
+        
+        
+    return -1;
     }
 }
