eb8e7a57fe7344b6944e7fbd2066e6ac276d2de4
Sai bargav Nellepalli <80017755+saibargavnellepalli@users.noreply.github.com>
2023-11-18 18:40:38 +0530

    Time: 2 ms (68.32%), Space: 45.4 MB (7.10%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,28 +1,18 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-       
+        
         int i=0;
-        int n = nums.length;
         
-        int newArray[] = new int[n];
-        
-        for(int j=0;j<n;j++){
-          
+        for(int j=0;j<nums.length;j++){
             if(nums[j] != 0){
-                newArray[i] = nums[j];
+                nums[i] = nums[j];
                 i++;
             }
         }
         
-        for(int k=i;k<n;k++){
-            newArray[k] = 0;
+        while(i<nums.length){
+            nums[i] = 0;
+            i++;
         }
-        
-        
-        
-        for(i=0;i<n;i++){
-            nums[i] = newArray[i];
-        }
-        
     }
 }
