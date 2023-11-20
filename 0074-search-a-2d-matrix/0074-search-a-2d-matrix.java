dafa2c47891728aad9e44dd67a4254d5b4dad5c8
Sai bargav Nellepalli <80017755+saibargavnellepalli@users.noreply.github.com>
2023-11-20 18:27:41 +0530

    Time: 0 ms (100.00%), Space: 41.5 MB (6.83%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,50 +1,19 @@
 class Solution {
-    public boolean searchMatrix(int[][] matrix, int k) {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        
         int n = matrix.length;
-        int m1 = matrix[0].length;
-        int s=0,e=n-1;
+        int m = matrix[0].length;
         
-        while(s<=e){
-            int m = (s+e)/2;
-            
-            if(k < matrix[m][0]){
-                e = m-1;
-            }
-            else if(k > matrix[m][m1-1]){
-                
-                s = m+1;
-            }
-            else{
-              return bs(m,matrix,k);
-            }
-            
-            
-        }
         
-        return false;
-    }
-    public boolean bs(int midInd, int matrix[][],int k){
-    
-        int start =0, end=matrix[0].length-1;
-        int mid;
-        while (start<=end){
-            //Making array half everytime
-            mid=(start+end)/2;
-
-            //checking in which part the element is present
-            if (matrix[midInd][mid]<k){
-                start=mid+1;
-            }
-            else if (matrix[midInd][mid]>k){
-                end=mid-1;
-            }
-            if (matrix[midInd][mid]==k)
-            {
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            if(matrix[i][j] == target){
                 return true;
             }
         }
+    }
         
         return false;
-      
-   } 
+       
+    }
 }
