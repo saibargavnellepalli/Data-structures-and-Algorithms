4e0cf93dcf614ad73369ffc3cac882b7cc1b0f03
Sai bargav Nellepalli <80017755+saibargavnellepalli@users.noreply.github.com>
2023-11-24 17:55:05 +0530

    Time: 0 ms (100.00%), Space: 40.5 MB (30.33%) - LeetHub

--- 0059-spiral-matrix-ii/0059-spiral-matrix-ii.java
+++ 0059-spiral-matrix-ii/0059-spiral-matrix-ii.java
@@ -1,44 +1,68 @@
 class Solution {
     public int[][] generateMatrix(int n) {
-       int ans[][] = new int[n][n];
         
-          int rs = 0, re = n-1;
-        int cs = 0, ce = n-1;
-        int elm = 1;
         
-        while(rs<=re && cs<=ce){
+        int[][] matrix = new int[n][n];
+        
+        
+        // we have to perform all 4 steps
+        
+        int m = matrix[0].length;
+        
+        
+        int rs=0, re=n-1;
+        int cs=0,ce = m-1;
+        
+        int elem = 1;
+        
+        
+        while(rs <= re && cs <= ce){
             
+        //step-1
         for(int i=cs; i<=ce; i++){
-           ans[rs][i] = elm;
-            elm++;
+            // ans.add(matrix[rs][i]);
+            
+            matrix[rs][i] = elem;
+            elem++;
         }
-        
         rs++;
         
-        for(int i=rs;i<=re;i++){
-        //ans.add(matrix[i][ce]);
-             ans[i][ce] = elm;
-            elm++;
-        }
         
+         //step-2
+        for(int i=rs; i<=re; i++){
+            // ans.add(matrix[i][ce]);
+            matrix[i][ce] = elem;
+            elem++;
+        }
         ce--;
         
-        for(int i=ce;i>=cs;i--){
+        //step-3
+        
+            
+        for(int i=ce; i>=cs; i--){
             // ans.add(matrix[re][i]);
-             ans[re][i] = elm;
-            elm++;
+            
+            matrix[re][i] = elem;
+            elem++;
         }
-        
         re--;
+            
+            
+            
         
-        for(int i=re;i>=rs;i--){
- ans[i][cs] = elm;
-            elm++;
+        //step -4
+        for(int i=re; i>=rs; i--){
+            // ans.add(matrix[i][cs]);
+           matrix[i][cs] = elem;
+            elem++;
+        }
+        cs++;
+        
         }
         
-        cs++;
-    }
-        return ans;
+        
+        
+        return matrix;
         
         
     }
