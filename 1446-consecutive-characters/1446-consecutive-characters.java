c70ae1e62016ed24873f6822ad2157041cabe78d
Sai bargav Nellepalli <80017755+saibargavnellepalli@users.noreply.github.com>
2023-11-29 16:16:44 +0530

    Time: 1 ms (100.00%), Space: 41 MB (44.41%) - LeetHub

--- 1446-consecutive-characters/1446-consecutive-characters.java
+++ 1446-consecutive-characters/1446-consecutive-characters.java
@@ -1,33 +1,39 @@
 class Solution {
     public int maxPower(String s) {
         
-        char prev = s.charAt(0);
+        //abceedffffr
+        
+        //leetcode
+        
+        //leetcordddde
+        
         
         int i=1;
-        int n = s.length();
-        int answer = 1;
+        int ans = 1;
         
-        while(i<n){
-           
-            if(s.charAt(i) == prev){
-                int curCount = 1;
+        while(i<s.length()){
+            
+            char prevElm = s.charAt(i-1);
+            char curElm = s.charAt(i);
+            
+            if(curElm == prevElm ){
                 
-                while(i<n && s.charAt(i) == prev){
-                    curCount++;
-                    prev = s.charAt(i);
+                int countOfSeq = 1;
+                
+                while(i<s.length() && s.charAt(i) == prevElm){
+                    countOfSeq++;
                     i++;
                 }
                 
-                answer = Math.max(answer,curCount);
+                ans = Math.max(ans, countOfSeq);
             }
             else{
-               
-                prev = s.charAt(i);
-                 i++;
+                i++;
             }
-            
         }
         
-        return answer;
+        return ans;
+        
+        
     }
 }
