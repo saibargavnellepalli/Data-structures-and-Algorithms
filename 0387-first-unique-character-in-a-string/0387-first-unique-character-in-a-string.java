375aeaf3e2a190821810589d5fbf545ed02aa6c2
Sai bargav Nellepalli <80017755+saibargavnellepalli@users.noreply.github.com>
2023-12-06 18:54:36 +0530

    Time: 31 ms (19.89%), Space: 44.7 MB (6.21%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
@@ -1,27 +1,51 @@
 class Solution {
     public int firstUniqChar(String s) {
+       
+        // "loveleetcode"
+        
+        // l - 2
+        // o - 2
+        // e - 4
+        // v - 1
+        // t - 1
+        // c - 1
+        // d - 1
         
         
-        HashMap<Character,Integer> map = new HashMap<>();
+          HashMap<Character , Integer > map = new HashMap<>();
+        int n = s.length();
         
-        for(int i=0;i<s.length();i++){
-           char ch = s.charAt(i);
+        for(int i=0;i<n;i++){
+            
+            char ch = s.charAt(i);
             
             if(map.containsKey(ch)){
                 int val = map.get(ch);
-                map.put(ch,val+1);
+                val+=1;
+                map.put(ch,val);
+             
             }
             else{
                 map.put(ch,1);
+                
             }
+         
         }
         
-        for(int i=0;i<s.length();i++){
-            if(map.get(s.charAt(i)) == 1){
+        
+        
+        for(int i=0;i<n;i++){
+            char ch = s.charAt(i);
+            if( map.get(ch) == 1){
                 return i;
             }
         }
         
         return -1;
+        
+        
+        
+        
+        
     }
 }
