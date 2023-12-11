7e1896c38fe307f2fe9980cc132047d7ec2e5207
Sai bargav Nellepalli <80017755+saibargavnellepalli@users.noreply.github.com>
2023-12-11 18:06:10 +0530

    Time: 2 ms (96.37%), Space: 44.2 MB (7.79%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
@@ -1,38 +1,54 @@
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
         
+        
+        /*
+        
+        create a hashmap and store the occurances of nums1 values
+        
+        traverse through the nums2, reduce the occurace of that element
+        
+        when occurance == 1, we can remove
+        
+        */
+        
+        
         HashMap<Integer,Integer> map = new HashMap<>();
         
-        for(int i=0;i<nums1.length;i++){
-            if(!map.containsKey(nums1[i])){
-                map.put(nums1[i], 1);
+        for(int cur : nums1){
+            
+            if(map.containsKey(cur)){
+                int val = map.get(cur);
+                val+=1;
+                map.put(cur, val);
             }
             else{
-                map.put(nums1[i], map.get(nums1[i])+1);
+                map.put(cur,1);
             }
         }
         
-        List<Integer> ans = new ArrayList<>();
+        
+          ArrayList<Integer> li = new ArrayList<>();
         
         for(int cur : nums2){
             
-              
-            
             if(map.containsKey(cur) && map.get(cur) >= 1){
-                ans.add(cur);
-                map.put(cur,map.get(cur)-1);
+                
+                li.add(cur);
+                
+                int val = map.get(cur);
+                val-=1;
+                map.put(cur,val);
             }
         }
         
+          int arr[] = new int[li.size()];
         
-        int arr[] = new int[ans.size()];
-        
-        for(int i=0;i<arr.length;i++){
-            arr[i] = ans.get(i);
-        }
+      for(int i=0;i<arr.length;i++){
+          
+          arr[i] = li.get(i);
+      }
         
         return arr;
     }
-    
-        
-    }
+}
