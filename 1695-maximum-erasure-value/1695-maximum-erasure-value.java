0000bf49b31c02a2ca8c17847c2f2bf33e781bf3
Sai bargav Nellepalli <80017755+saibargavnellepalli@users.noreply.github.com>
2023-12-11 19:28:05 +0530

    Time: 51 ms (45.28%), Space: 55.9 MB (91.44%) - LeetHub

--- 1695-maximum-erasure-value/1695-maximum-erasure-value.java
+++ 1695-maximum-erasure-value/1695-maximum-erasure-value.java
@@ -1,38 +1,38 @@
 class Solution {
     public int maximumUniqueSubarray(int[] nums) {
         
-        HashMap<Integer,Integer> map = new HashMap<>();
-        int n = nums.length;
-        int rel = 0;        
-        int ans = 0;
-        int acq=0;
-        int start = 0, end=0;
         
+           
+        HashMap<Integer,Integer>  map = new HashMap<>();
+        int acq=0, rel=0;
+        int ans = 0;
         int sum = 0;
         
-        for( acq=0; acq<n; acq++){
-                
-            int ch = nums[acq];
-            
-            if(map.containsKey(ch)){
-                
-                while(rel < n && map.containsKey(ch)){
-                    map.remove(nums[rel]);
-                    sum-=nums[rel];
-                    rel++;
-                }
+        int n = nums.length;
+        
+       for(acq =0; acq<n; acq++){
+           
+           int curElm = nums[acq];
+           
+           //substring is invalid
+           if(map.containsKey(curElm)){
                
-            }
-                map.put(ch,acq);
-                sum += ch;
-                ans = Math.max(sum,ans);
-            
-        }
-        
-        
-
+               while(rel< n && map.containsKey(curElm)){
+                   map.remove(nums[rel]);
+                   sum -= nums[rel];
+                   rel++;
+               }
+           }
+           
+           //substring is valid
+           map.put(curElm,acq);
+           
+           sum += curElm;
+           
+           ans = Math.max(sum, ans);
+           
+       }
         
         return ans;
-        
     }
 }
