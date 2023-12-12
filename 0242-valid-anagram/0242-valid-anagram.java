f2a3978c8d7255241bff2b9d6c5eb8fdc7aa31bc
Sai bargav Nellepalli <80017755+saibargavnellepalli@users.noreply.github.com>
2023-12-12 19:17:59 +0530

    Time: 19 ms (15.71%), Space: 43.5 MB (55.27%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -3,38 +3,37 @@ class Solution {
         
         
         if(s.length() != t.length()) return false;
-                    
-              HashMap<Character,Integer> map = new HashMap<>();
         
-              for(int i=0;i<s.length();i++){
-                  
-                  char ch = s.charAt(i);
-                  
-                  if(map.containsKey(ch)){
-                      map.put(ch,map.get(ch)+1);
-                  }
-                  else{
-                      map.put(ch,1);
-                  }
-              }
+        HashMap<Character,Integer> map = new HashMap<>();
         
         
-         for(int i=0;i<t.length();i++){
-                  
-                  char ch = t.charAt(i);
-                  
-                  if(map.containsKey(ch)  && map.get(ch) >= 1){
-                      map.put(ch,map.get(ch)-1);
-                  }
-             else{
-                 return false;
-             }
-                  
-              }
+        for(int i=0;i<s.length();i++){
+            char ch = s.charAt(i);
+            
+            if(map.containsKey(ch)){
+                map.put(ch, map.get(ch)+1);
+            }
+            else{
+                map.put(ch,1);
+            }
+        }
         
         
-          return true;
-         
-    
+        for(int i=0;i<t.length(); i++){
+            char ch = t.charAt(i);
+            
+            
+            if(map.containsKey(ch)  && map.get(ch) >= 1){
+                
+                map.put(ch, map.get(ch)-1);
+            }
+            else{
+                return false;
+            }
+        }
+        
+        return true;
+        
+        
     }
 }
