95da7becf18e13c975f966202c69a359a96fec93
Sai bargav Nellepalli <80017755+saibargavnellepalli@users.noreply.github.com>
2023-12-21 16:51:53 +0530

    Time: 4 ms (56.42%), Space: 44.4 MB (8.08%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,50 +1,39 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         
-         Stack<Integer> st = new Stack<>();
-        int pre[] = new int[nums2.length];
         
-        for(int i=nums2.length-1;i>=0;i--){
+        int nge[] = new int[nums2.length];
+        
+        Stack<Integer> st = new Stack<>();
+        
+        for(int i=0; i<nums2.length; i++){
             
-            if(st.isEmpty()){
-                st.push(nums2[i]);
-                pre[i] = -1;
-            }
-            else{
-                if(nums2[i] < st.peek()){
-                    pre[i] = st.peek();
-                    st.push(nums2[i]);
-                }
-                else{
-                    while(st.isEmpty()==false && st.peek() < nums2[i]){
-                        st.pop();
-                    }
-                    if(st.isEmpty())
-                        pre[i] = -1;
-                    else
-                    pre[i] = st.peek();
-                    st.push(nums2[i]);
-                }
-            }
+               while(!st.isEmpty()  &&  nums2[i] > nums2[st.peek()] ){
+                   nge[st.peek()] = nums2[i];
+                   
+                   st.pop();
+               }
             
-     }
+            st.push(i);
+        }
+        
         
         HashMap<Integer,Integer> map = new HashMap<>();
         
-        for(int i=0;i<nums2.length;i++){
-            map.put(nums2[i],i);
+        for(int i=0; i<nums2.length;i++){
+            
+            if(nge[i] == 0) nge[i] = -1;
+            
+            map.put(nums2[i], nge[i]);
         }
         
         int ans[] = new int[nums1.length];
-        for(int i=0;i<nums1.length;i++){
-           int ind = map.get(nums1[i]);
-            ans[i] = pre[ind];
+        
+        for(int i=0;i<nums1.length; i++){
+             ans[i] = map.get(nums1[i]);
         }
         
         return ans;
         
     }
-    /*
-   
-        */
 }
