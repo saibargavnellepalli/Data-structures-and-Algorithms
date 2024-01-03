3b2c7324a8c0c78d913fa704efd8a148ce3c7d74
Sai bargav Nellepalli <80017755+saibargavnellepalli@users.noreply.github.com>
2024-01-03 14:49:40 +0530

    Time: 1 ms (93.62%), Space: 45.5 MB (26.03%) - LeetHub

--- 0203-remove-linked-list-elements/0203-remove-linked-list-elements.java
+++ 0203-remove-linked-list-elements/0203-remove-linked-list-elements.java
@@ -10,7 +10,8 @@
  */
 class Solution {
     public ListNode removeElements(ListNode head, int val) {
-        
+      
+        /*
         ListNode dummy = new ListNode(-1);
        dummy.next = head;
         ListNode cur = dummy;
@@ -26,7 +27,24 @@ class Solution {
         }
         
         return dummy.next;
+       */
         
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        
+        ListNode cur = dummy;
+        
+        while(cur != null && cur.next != null){
+            
+            if(cur.next.val == val){
+                cur.next = cur.next.next;
+            }
+            else{
+                cur = cur.next;
+            }
+        }
+        
+        return dummy.next;
         
         
     }
