d48b0c609b522ae49c740321d5ac9a11235f9dd3
Sai bargav Nellepalli <80017755+saibargavnellepalli@users.noreply.github.com>
2024-01-09 19:28:49 +0530

    Time: 0 ms (100.00%), Space: 42.5 MB (32.00%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,32 +1,30 @@
 class Solution {
+    
     List<List<Integer>> ans = new ArrayList<>();
     
     public List<List<Integer>> subsets(int[] nums) {
-            
+        
         List<Integer> list = new ArrayList<>();
         sub(0, nums, list);
         
         return ans;
     }
     
-    public void sub(int i, int[] nums, List<Integer> list){
+    public void sub(int i, int nums[], List<Integer> list){
         
         if(i == nums.length){
             ans.add(new ArrayList<>(list));
             return;
         }
         
+        //take
         list.add(nums[i]);
-        
         sub(i+1, nums, list);
         
+        
+        //removing/missing
         int n = list.size();
-        
         list.remove(n-1);
-        
         sub(i+1, nums, list);
-        
-        
-        
     }
 }
