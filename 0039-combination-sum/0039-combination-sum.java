7c792403746b6d40f72e1d4ba1ff6a78565cd6db
Sai bargav Nellepalli <80017755+saibargavnellepalli@users.noreply.github.com>
2024-01-09 17:43:49 +0530

    Time: 2 ms (80.20%), Space: 44.6 MB (17.55%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,35 +1,34 @@
 class Solution {
-     List<List<Integer>> ans = new ArrayList<>();
+    
+    List<List<Integer>> ans = new ArrayList<>();
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<Integer> list = new ArrayList<>();
         
-        combinations(0,0,candidates,target,list);
+        List<Integer> list = new ArrayList<>();
+        combi(0,0,candidates, target, list);
+        
         return ans;
     }
     
-    public void combinations(int i, int sum, int[] arr, int k, List<Integer> list){
+    
+    public void combi(int i, int sum, int[] arr, int k, List<Integer> list){
         
-        //System.out.println(sum);
-         if(sum==k){
-                ans.add(new ArrayList<>(list));
-            }
-        if(sum >= k) return;
+        if(sum == k){
+            ans.add(new ArrayList<>(list));
+        }
         
-        
-        if(i==arr.length){
-           
+        if(sum >= k|| i == arr.length){
             return;
         }
         
         list.add(arr[i]);
         sum += arr[i];
-        
-        combinations(i,sum,arr,k,list);
+        combi(i, sum, arr, k, list);
         
         int n = list.size();
-        sum-=arr[i];
+        sum -= arr[i];
         list.remove(n-1);
         
-        combinations(i+1,sum,arr,k,list);
+        combi(i+1, sum, arr,k, list);
+        
     }
 }
