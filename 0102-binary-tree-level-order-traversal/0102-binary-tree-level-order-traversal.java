e4677f47f95c964767746104fe2e282b593f8594
Sai bargav Nellepalli <80017755+saibargavnellepalli@users.noreply.github.com>
2024-01-19 17:16:32 +0530

    Time: 1 ms (89.87%), Space: 45.2 MB (5.85%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
@@ -1,44 +1,32 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
-        List<List<Integer>> ans = new ArrayList<>();
-        Queue<TreeNode> q = new LinkedList<>();
-        if(root != null)
-            q.add(root);
-        else
-            return ans;
+          List<List<Integer>> ans = new ArrayList<>();
         
-        while(!q.isEmpty()){
+        if(root == null) return ans;
+        
+        Queue<TreeNode> q = new LinkedList<>();
+        
+        q.offer(root);
+        
+        while(q.isEmpty() == false){
+            
             int n = q.size();
-            ArrayList<Integer> al = new ArrayList<>();
-            for(int i=0;i<n;i++){
-                TreeNode temp = q.remove();
-                al.add(temp.val);
+            
+            List<Integer> list = new ArrayList<>();
+            
+            for(int i=1; i<=n; i++){
                 
-                if(temp.left != null){
-                    q.add(temp.left);
-                }
-                if(temp.right != null){
-                    q.add(temp.right);
-                }
+                TreeNode temp = q.poll();
                 
+                if(temp.left != null) q.offer(temp.left);
+                if(temp.right != null) q.offer(temp.right);
+                
+                list.add(temp.val);
             }
-            ans.add(al);
+            ans.add(list);
         }
+        
+        
         
         return ans;
     }
