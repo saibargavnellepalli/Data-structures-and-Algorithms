9b2bd504794699d3c729e684d164691900dc1956
Sai bargav Nellepalli <80017755+saibargavnellepalli@users.noreply.github.com>
2024-01-25 18:51:35 +0530

    Time: 16 ms (85.10%), Space: 45.9 MB (69.55%) - LeetHub

--- 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.java
+++ 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.java
@@ -15,49 +15,50 @@
  */
 class Solution {
     public List<Integer> getAllElements(TreeNode root1, TreeNode root2) {
-      
+     
         List<Integer> list1 = new ArrayList<>();
         List<Integer> list2 = new ArrayList<>();
+        
         List<Integer> ans = new ArrayList<>();
         
-
         inorder(root1, list1);
+        
         inorder(root2, list2);
+        
         int i=0, j=0;
         
         while(i<list1.size() && j<list2.size()){
-        if(list1.get(i) <= list2.get(j)){
-            ans.add(list1.get(i));
-            i++;
-        }
-        else{
-             ans.add(list2.get(j));
-             j++; 
-        }
             
-           
-    }
-         while(i<list1.size()) {
+            if(list1.get(i) <= list2.get(j)){
                 ans.add(list1.get(i));
                 i++;
             }
-            
-              while(j<list2.size()) {
+            else{
                 ans.add(list2.get(j));
                 j++;
             }
+        }
+        
+        while(i<list1.size()) ans.add(list1.get(i++));
+        
+        while(j<list2.size()) ans.add(list2.get(j++));
         
         return ans;
         
+        
+        
+        
+        
+        
     }
-     public void inorder(TreeNode root , List<Integer> list){
-        if(root == null) return ;
-
-        inorder(root.left , list);
+    
+    public void inorder(TreeNode root, List<Integer> list){
+        
+        if(root == null) return;
+        
+        inorder(root.left, list);
         list.add(root.val);
         inorder(root.right, list);
-
-        return;
+        
     }
-     
 }
