19254eeb59636f9abaf8a1b814db50312ebdd3c0
Sai bargav Nellepalli <80017755+saibargavnellepalli@users.noreply.github.com>
2024-01-25 19:42:28 +0530

    Time: 8 ms (24.75%), Space: 45.1 MB (32.40%) - LeetHub

--- 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.java
+++ 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.java
@@ -15,45 +15,51 @@
  */
 class Solution {
     public int[] findMode(TreeNode root) {
+        
         HashMap<Integer,Integer> map = new HashMap<>();
         
         inorder(root, map);
+        
         int max = 0;
         for(int val : map.values()){
-            max = Math.max(val,max);
-        }
-        int n = 0;
-        for(int val : map.values()){
-          if(val == max) n++;
-            
+            if(val > max) max = val;
         }
         
-        int arr[] = new int[n];
+        int n = 0;
+        for(int val : map.values()){
+            if(val == max) n++;
+        }
+        
+        int ans[] = new int[n];
         int i=0;
+        
         for(int key : map.keySet()){
-            if(map.get(key) == max){
-                arr[i++] = key;
+            int val = map.get(key);
+            if(val == max){
+                ans[i] = key;
+                i++;
             }
         }
         
-        return arr;
+        return ans;
+        
         
     }
     
-      public void inorder(TreeNode root , HashMap<Integer,Integer> map){
-        if(root == null) return ;
-
-        inorder(root.left , map);
+    public void inorder(TreeNode root, HashMap<Integer,Integer> map){
+        
+        if(root == null) return;
+        
+        inorder(root.left, map);
+        
         if(map.containsKey(root.val)){
-            map.put(root.val, map.get(root.val)+1);
+             int val = map.get(root.val);
+             map.put(root.val, val+1);
         }
-          else{
-              map.put(root.val, 1);
-          }
-          
+        else{
+            map.put(root.val, 1);
+        }
+        
         inorder(root.right, map);
-
-        return;
     }
-    
 }
