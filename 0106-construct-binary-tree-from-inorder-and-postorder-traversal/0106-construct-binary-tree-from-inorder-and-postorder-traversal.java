ff951d1ef082e3335aca21436b894403b9d4d8e0
Sai bargav Nellepalli <80017755+saibargavnellepalli@users.noreply.github.com>
2024-01-26 18:04:16 +0530

    Time: 2 ms (67.59%), Space: 44.1 MB (60.34%) - LeetHub

--- 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.java
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.java
@@ -1,42 +1,40 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
-    public TreeNode buildTree(int[] inorder, int[] postorder) {
-        HashMap<Integer,Integer> map = new HashMap<>();
-         buildMap(map,inorder); 
-        int arr[] = {postorder.length-1};
-       return treeFun(0,inorder.length-1,inorder,postorder,map,arr);
+     HashMap<Integer,Integer> map = new HashMap<>();
+    public TreeNode buildTree(int[] inorder, int[] preorder) {
+         int[] preInd = {preorder.length-1};
+        
+       
+        for(int i=0;i<inorder.length;i++)
+        map.put(inorder[i], i);
+        
+        return f(preorder, inorder, preInd, 0, inorder.length-1);
+        
     }
     
-    public void buildMap(HashMap<Integer,Integer> map, int[] arr){
-        
-        for(int i=0;i<arr.length;i++)
-            map.put(arr[i],i);
-    }
     
-    public TreeNode treeFun(int st, int end, int[] in, int[] post, HashMap<Integer,Integer> map, int[] arr){
-        if(st > end || arr[0]<0) return null;
+    
+    public TreeNode f(int[] preorder, int[] inorder, int[] preInd, int startInd, int endInd){
+           
+        if(preInd[0] < 0 || startInd > endInd){
+            return null;
+        }
         
-        int cur = post[arr[0]];
-        arr[0]-=1;
         
-        TreeNode node = new TreeNode(cur);
-        node.right = treeFun(map.get(cur)+1,end,in,post,map,arr);
-        node.left = treeFun(st,map.get(cur)-1,in,post,map,arr);
+        int curVal = preorder[preInd[0]];
+        TreeNode root = new TreeNode(curVal);
+        preInd[0]-=1;
         
-        return node;
+        
+        
+       
+        root.right = f(preorder, inorder, preInd, map.get(curVal)+1, endInd);
+         root.left = f(preorder, inorder, preInd, startInd, map.get(curVal)-1);
+        
+        return root;
+        
+        
+        
+        
+        
     }
 }
