4f37f9c72126f79376be763b64d71c83fe470b5c
Sai bargav Nellepalli <80017755+saibargavnellepalli@users.noreply.github.com>
2024-01-26 17:49:16 +0530

    Time: 3 ms (47.10%), Space: 44.5 MB (30.09%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -1,51 +1,37 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+
 class Solution {
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-     HashMap<Integer,Integer> store = new HashMap<>();
         
-        buildMap(store,inorder);
-        int ind[] = {0};
+        int[] preInd = {0};
+        return f(preorder, inorder, preInd, 0, inorder.length-1);
         
-        return construct(ind,0,inorder.length-1,preorder,inorder,store);
-       
     }
     
-     public void buildMap(HashMap<Integer,Integer> map, int[] nums){
-            int i=0;
-            for(int cur : nums){
-                map.put(cur,i);
-                i++;
-            }
+    public TreeNode f(int[] preorder, int[] inorder, int[] preInd, int startInd, int endInd){
+           
+        if(preInd[0] >= preorder.length || startInd > endInd){
+            return null;
         }
-    
-    public TreeNode construct(int[] arr, int s, int e, int[] p, int[] i, HashMap<Integer,Integer> map){
         
-        if(arr[0] >= i.length || s>e) return null;
         
-        int cur = p[arr[0]];
-        arr[0] += 1;
+        int curVal = preorder[preInd[0]];
+        TreeNode root = new TreeNode(curVal);
+        preInd[0]+=1;
         
-        TreeNode root = new TreeNode(cur);
-        int ind = map.get(cur);
+        int i=-1;
         
-        root.left = construct(arr,s,ind-1,p,i,map);
-        root.right = construct(arr,ind+1,e,p,i,map);
+        for( i=startInd; i<=endInd; i++){
+            if(inorder[i] == curVal) break;
+        }
+        
+        root.left = f(preorder, inorder, preInd, startInd, i-1);
+        root.right = f(preorder, inorder, preInd, i+1, endInd);
         
         return root;
+        
+        
+        
+        
         
     }
 }
