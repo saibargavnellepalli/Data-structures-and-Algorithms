e36befa3841ecb61ea1d57fd55664c48c40f77fa
Sai bargav Nellepalli <80017755+saibargavnellepalli@users.noreply.github.com>
2024-01-31 00:07:54 +0530

    Time: 8 ms (6.75%), Space: 12 MB (9.54%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -7,8 +7,13 @@ public:
         
         
         for(int i=0; i<n; i++){
+            
+            if(target>= matrix[i][0] && target <= matrix[i][m-1]){
+            
             for(int j=0; j<m; j++){
                 if(matrix[i][j] == target) return true;
+            }
+                
             }
         }
         
