ec4381b1630ab11bf71b1b00ee39a80ea5fae786
Sai bargav Nellepalli <80017755+saibargavnellepalli@users.noreply.github.com>
2024-01-30 19:49:15 +0530

    Time: 0 ms (100.00%), Space: 40.2 MB (62.59%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,22 +1,23 @@
 class Solution {
     public int climbStairs(int n) {
+        
         Integer dp[] = new Integer[n+1];
         
-       return f(0,n,dp);
+        return f(0, n, dp);
+        
     }
     
-    public int f(int cur, int tar, Integer[] dp){
-        
+    public int f(int cur, int tar, Integer dp[]){
         
         if(cur == tar) return 1;
         
-        if(cur>tar) return 0;
+        if(cur > tar) return 0;
         
-        if(dp[cur] != null) return dp[cur];
+         if(dp[cur] != null) return dp[cur];
         
-        int one = f(cur+1, tar, dp);
-        int two = f(cur+2, tar, dp);
+        int onestep = f(cur+1, tar, dp);
+        int twosteps = f(cur+2, tar, dp);
         
-        return dp[cur] = one+two;
+        return dp[cur] = onestep+twosteps;
     }
 }
