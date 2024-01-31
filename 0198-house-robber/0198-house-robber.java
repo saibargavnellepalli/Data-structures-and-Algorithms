3e4c71fc0c14100579aadeb3f8efcc81175d9e9a
Sai bargav Nellepalli <80017755+saibargavnellepalli@users.noreply.github.com>
2024-01-31 19:30:16 +0530

    Time: 0 ms (100.00%), Space: 41 MB (46.19%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,23 +1,22 @@
 class Solution {
     public int rob(int[] nums) {
         
-        Integer[] dp = new Integer[nums.length+1];
+        Integer dp[] = new Integer[nums.length+1];
         
-       return f(0, nums, dp);
-        
+        return f(0, nums, dp);
     }
     
-    public int f(int curInd, int[] nums, Integer[] dp){
+    public int f(int curH, int nums[], Integer dp[]){
         
-        if(curInd >= nums.length) return 0;
-        
-        if(dp[curInd] != null) return dp[curInd];
-        
-        int rob = nums[curInd]+f(curInd+2, nums, dp);
-        int not = f(curInd+1, nums, dp);
+        if(curH >= nums.length) return 0;
         
         
-        return dp[curInd] = Math.max(rob,not);
+        if(dp[curH] != null) return dp[curH];
         
+        int rob = nums[curH] + f(curH+2, nums, dp);
+        int not = f(curH+1, nums, dp);
+        
+        
+        return dp[curH] = Math.max(rob, not);
     }
 }
