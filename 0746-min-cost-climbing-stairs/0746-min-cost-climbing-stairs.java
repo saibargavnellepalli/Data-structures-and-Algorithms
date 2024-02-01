f85060777339815b019253cb5f7e7c58b397837d
Sai bargav Nellepalli <80017755+saibargavnellepalli@users.noreply.github.com>
2024-02-01 18:28:07 +0530

    Time: 1 ms (49.03%), Space: 42.9 MB (81.05%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -1,28 +1,16 @@
 class Solution {
     public int minCostClimbingStairs(int[] cost) {
-        
-        Integer dp1[] = new Integer[cost.length+1];
-        
-        int a =  f(0, cost.length, cost,dp1);
-        
-          Integer dp2[] = new Integer[cost.length+1];
-        int b =  f(1, cost.length, cost, dp2);
-        
-        return Math.min(a,b);
+        Integer[] dp = new Integer[cost.length];
+        int zero =  f(0, cost, dp);
+        int one =  f(1, cost, dp);
+        return Math.min(one, zero);   
     }
-    
-    public int f(int cur, int tar, int[] cost, Integer dp[]){
-        
-        if(cur == tar) return 0;
-        
-        if(cur > tar) return 10000;
-        
-        
-        if(dp[cur] != null) return dp[cur];
-        
-        int one = cost[cur]+f(cur+1, tar, cost, dp);
-        int two = cost[cur]+f(cur+2, tar, cost, dp);
-        
-        return dp[cur] = Math.min(one, two);
+    public int f(int curSt, int[] cost, Integer[] dp)
+    {
+        if(curSt >= cost.length) return 0;
+        if(dp[curSt] != null) return dp[curSt];
+        int oneS = cost[curSt] +  f(curSt + 1, cost, dp);
+        int twoS =  cost[curSt] + f(curSt + 2, cost, dp);
+        return dp[curSt] = Math.min(oneS, twoS);
     }
-}
+    }
