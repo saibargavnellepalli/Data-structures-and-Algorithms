04be6ee8d44f2e6a0b00f363222ae11b1ba1fbeb
Sai bargav Nellepalli <80017755+saibargavnellepalli@users.noreply.github.com>
2024-02-02 19:36:57 +0530

    Time: 15 ms (18.04%), Space: 46.4 MB (50.56%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,26 +1,27 @@
 class Solution {
     public int change(int amount, int[] coins) {
         
-        Integer dp[][] = new Integer[coins.length+1][100002];
+        Integer dp[][] = new Integer[coins.length+1][amount+1];
         
         return f(0, amount, coins, dp);
+        
     }
     
     public int f(int curInd, int amount, int[] coins, Integer dp[][]){
         
         if(amount == 0) return 1;
         
-        if(curInd == coins.length || amount < 0) return 0;
+        if(curInd >= coins.length) return 0;
         
+        if(amount < 0) return 0;
         
         if(dp[curInd][amount] != null) return dp[curInd][amount];
         
-        int consider = f(curInd, amount-coins[curInd], coins, dp);
+        int take = f(curInd, amount-coins[curInd], coins, dp);
+        int not = f(curInd+1,amount,coins, dp);
         
-        int notconsider = f(curInd+1, amount, coins, dp);
         
-        
-        return dp[curInd][amount] = consider+notconsider;
+        return dp[curInd][amount] = take+not;
         
     }
 }
