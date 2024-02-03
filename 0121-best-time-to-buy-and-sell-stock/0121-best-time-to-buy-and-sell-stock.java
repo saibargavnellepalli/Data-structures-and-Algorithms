c50af560ede38d60bc0a8de93fdfbe75bcf6b59d
Sai bargav Nellepalli <80017755+saibargavnellepalli@users.noreply.github.com>
2024-02-03 19:38:09 +0530

    Time: 173 ms (7.72%), Space: 97.4 MB (8.59%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,24 +1,45 @@
 class Solution {
     public int maxProfit(int[] prices) {
+        Integer[][][] dp = new Integer[prices.length+1][2][3];
         
         
-        // 7 1 5 3 6 4
-          // 7  1 1 1 1 1
-               
-          int preMin[] = new int[prices.length];
-          int min = prices[0];
+        return f(0, 1, 1, prices, dp);
         
-        for(int i=0;i<prices.length;i++){
-            min = Math.min(min,prices[i]);
-            preMin[i] = min;
+    }
+    
+    public int f(int ind, int canBuy, int k, int[] prices, Integer[][][] dp){
+        
+        
+        //bases cases
+        if(ind >= prices.length  || k == 0) return 0;
+        
+        
+        if(dp[ind][canBuy][k] != null) return dp[ind][canBuy][k];
+        
+        //recurive calls 
+        //have the access to buy on "cur index" day
+        int profit = 0;
+        
+        if(canBuy == 1){
+            
+            int buy = -prices[ind] + f(ind+1, 0,k, prices, dp);
+            int not = 0 + f(ind+1, 1,k, prices, dp);
+            
+            profit = Math.max(buy, not);
+        }
+        //have the access to sell
+        else{
+            
+            int sell = prices[ind] + f(ind+1, 1,k-1, prices, dp);
+            int not = 0 + f(ind+1, 0,k, prices, dp);
+            
+            profit = Math.max(sell, not);
         }
         
-        int ans = 0;
         
-        for(int i=0;i<prices.length;i++){
-            ans = Math.max(ans,prices[i]-preMin[i]);
-        }
+        //return answer
+        return dp[ind][canBuy][k] = profit;
         
-        return ans;
+        
     }
 }
