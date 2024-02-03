58b65e18ec7de6a816e4d74cfba028a4bd4c950f
Sai bargav Nellepalli <80017755+saibargavnellepalli@users.noreply.github.com>
2024-02-03 17:22:24 +0530

    Time: 0 ms (100.00%), Space: 40.6 MB (18.68%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+        
+        Integer dp[][] = new Integer[m+1][n+1];
+        
+        return f(0,0,m,n, dp);
+    }
+    
+    
+    public int f(int i, int j, int m, int n, Integer dp[][]){
+        
+        if(i==m-1 && j==n-1) return 1;
+        
+        
+        if(i >=m || j>=n)  return 0;
+        
+        
+        if(dp[i][j] != null) return dp[i][j];
+        
+        
+        int right = f(i, j+1, m, n, dp);
+        int down = f(i+1, j, m, n, dp);
+        
+        return dp[i][j] = right+down;
+        
+    }
+}
