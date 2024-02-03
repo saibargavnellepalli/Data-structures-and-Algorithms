c140ee7dc930ac820e7caae53616772ddfc27c8e
Sai bargav Nellepalli <80017755+saibargavnellepalli@users.noreply.github.com>
2024-02-03 19:53:28 +0530

    Time: 0 ms (100.00%), Space: 40.7 MB (12.89%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -3,25 +3,22 @@ class Solution {
         
         Integer dp[][] = new Integer[m+1][n+1];
         
-        return f(0,0,m,n, dp);
+       return f(0, 0, m,n, dp); 
+        
     }
-    
     
     public int f(int i, int j, int m, int n, Integer dp[][]){
         
         if(i==m-1 && j==n-1) return 1;
         
-        
-        if(i >=m || j>=n)  return 0;
-        
+        if(i>=m || j>=n ) return 0;
         
         if(dp[i][j] != null) return dp[i][j];
         
+        int right = f(i+1, j, m, n, dp);
+        int down = f(i, j+1, m,n, dp);
         
-        int right = f(i, j+1, m, n, dp);
-        int down = f(i+1, j, m, n, dp);
         
-        return dp[i][j] = right+down;
-        
+        return dp[i][j]= right+down;
     }
 }
