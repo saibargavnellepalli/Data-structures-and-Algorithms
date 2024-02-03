971334b0a9db28205432bffda0b7f9ed301067c6
Sai bargav Nellepalli <80017755+saibargavnellepalli@users.noreply.github.com>
2024-02-03 19:31:46 +0530

    Time: 9 ms (35.34%), Space: 49.6 MB (5.45%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
@@ -1,26 +1,45 @@
 class Solution {
     public int maxProfit(int k, int[] prices) {
+    Integer[][][] dp = new Integer[prices.length+1][2][k+1];
         
-     Integer dp[][][] = new Integer[prices.length][2][k+1];
         
-        return f(0,1,k,prices,dp);
+        return f(0, 1, k, prices, dp);
+        
     }
     
-    public int f(int ind, int canBuy, int k,int[] prices, Integer dp[][][]){
+    public int f(int ind, int canBuy, int k, int[] prices, Integer[][][] dp){
         
-        if(ind == prices.length || k == 0) return 0;
+        
+        //bases cases
+        if(ind >= prices.length  || k == 0) return 0;
+        
         
         if(dp[ind][canBuy][k] != null) return dp[ind][canBuy][k];
         
-       int profit = 0;
+        //recurive calls 
+        //have the access to buy on "cur index" day
+        int profit = 0;
         
         if(canBuy == 1){
-           profit =  Math.max(-prices[ind] + f(ind+1,0,k,prices,dp), 0+f(ind+1,1,k,prices,dp));
+            
+            int buy = -prices[ind] + f(ind+1, 0,k, prices, dp);
+            int not = 0 + f(ind+1, 1,k, prices, dp);
+            
+            profit = Math.max(buy, not);
         }
+        //have the access to sell
         else{
-       profit = Math.max(prices[ind]+f(ind+1,1,k-1,prices,dp), 0+f(ind+1,0,k,prices,dp));                           
-         }
-                              
-          return dp[ind][canBuy][k] = profit;
+            
+            int sell = prices[ind] + f(ind+1, 1,k-1, prices, dp);
+            int not = 0 + f(ind+1, 0,k, prices, dp);
+            
+            profit = Math.max(sell, not);
+        }
+        
+        
+        //return answer
+        return dp[ind][canBuy][k] = profit;
+        
+        
     }
 }
